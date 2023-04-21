bd3749392a38b1b8dd53afa57f1f6e9e00aa7382
Ken <kabutukenneth3@gmail.com>
2023-04-21 13:19:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const remMain = document.querySelector('#main');
+
+remMain.remove('#main');
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'Ken Wachira';
+newHeader.innerHTML = `${yourName}, is the champion`; 
